6dcf3b05f716d5428b19d7f2be9c7beb2fed9faf
Bohn <y2742@softwareag.com>
2020-05-12 15:16:13 +0200

    Cleaning Up the timer (439)

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -7,23 +7,23 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hooks-cb589.firebaseio.com/ingredients.json')
-      .then(
-        response => {
-          return response.json()
-        }).then(responseData => {
-          const loadedIngredients = [];
-          for (const key in responseData) {
-            loadedIngredients.push({
-              id: key,
-              title: responseData[key].title,
-              amount: responseData[key].amount
-            });
-          }
-          setUserIngredients(loadedIngredients);
-        });
-  },[]);
+  // useEffect(() => {
+  //   fetch('https://react-hooks-cb589.firebaseio.com/ingredients.json')
+  //     .then(
+  //       response => {
+  //         return response.json()
+  //       }).then(responseData => {
+  //         const loadedIngredients = [];
+  //         for (const key in responseData) {
+  //           loadedIngredients.push({
+  //             id: key,
+  //             title: responseData[key].title,
+  //             amount: responseData[key].amount
+  //           });
+  //         }
+  //         setUserIngredients(loadedIngredients);
+  //       });
+  // },[]);
 
   useEffect(() => {
     console.log('RENDERING INGREDIENTS', userIngredients);
